63d97aaa397f7052cadfb8a56ef560f7463956c7
Sylwia Waligóra <sylwia.waligora@ageno.pl>
2022-03-08 12:44:28 +0100

    Remove some variables

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -1,18 +1,11 @@
-// zmienne globalne, funcke jak w csss, helpery
-
 export const theme = {
   colors: {
     white: '#F4F5F9',
-    lightGrey: '#F7F8FA',
     grey: '#C0C7D6',
-    darkGrey: '#737C8E',
     black: '#161D28',
     success: '#8FCB81',
     error: '#FF8383',
     warning: '#E1D888',
-    darkPurple: '#C0C7D6',
-    lightPurple: '#eff4fc',
-    lightGreen: '#CFECF4'
   },
   fontSize: {
     xxl: '34px',
